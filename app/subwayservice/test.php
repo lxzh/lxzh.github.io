0301e3d9c5f7d2957181bc7067f2199618ff99f1
ljf1239848066 <1239848066@qq.com>
2016-04-08 00:23:49 +0800

    init

--- app/subwayservice/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-mysql_connect("115.239.229.249","a1007225618","67912333");
-mysql_select_db("a1007225618");
-  $q=mysql_query("SELECT * subwayservice WHERE birthyear");
-  while($e=mysql_fetch_assoc($q))
-        $output[]=$e; 
-print(json_encode($output)); 
-mysql_close();
-?>
